ab322e2290550ecf703bc66db92130ada0c6b6be
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-06-17 22:24:06 +0700

    Cleanup & Improvements

--- README.md
+++ README.md
@@ -1,44 +1,5 @@
-<h1>RevivalTax<img src="https://github.com/NhanAZ/RevivalTax/blob/main/images/icon.png" height="64" width="64"  align="left"></img></h1><br/>
-
-[![](https://poggit.pmmp.io/shield.state/RevivalTax)](https://poggit.pmmp.io/p/RevivalTax)
-
-[![](https://poggit.pmmp.io/shield.api/RevivalTax)](https://poggit.pmmp.io/p/RevivalTax)
-
-The player will be deducted the revival tax on death. A useful plugin for RPG survival servers?
-
-# Setup
-How to setup? Very simple! Follow the steps below:
-- Step 1: Download the plugin and put it in plugins`(PocketMine-MP/plugins)`
-- Step 2: Start the server to load `config.yml`
-- Step 3: After server startup is complete, stop the server.
-- Step 4: Go to the path `PocketMine-MP/plugin_data/RevivalTax/config.yml`
-- Step 5: Then edit tax percentage and tax withholding notice at `config.yml`
-
-# Images
-<img src="https://github.com/NhanAZ/RevivalTax/blob/main/images/ingame.png" />
-
-# Config
-## config.yml
-```
----
-# Percentage of tax amount deducted by player after revival
-# The player's deducted amount is calculated using the formula below:
-# (Player's Money) x TaxPercent(%) / 100 = (Player taxes are deducted when they revival)
-TaxPercent: 10 #Percent(%)
-
-# Notifications sent to players when they death
-# {TaxMoney} : Server revival tax percentage
-# {TaxPercent} The amount of the player's revival tax to pay
-TaxNotice: "§aYou have been deducted §e${TaxMoney} §arevival tax! §f(§bTax Percent: §c{TaxPercent}%§f)§r"
-...
-```
+# General
+**The player will be deducted the revival tax on death.**
 
 # Contact
-If you encounter an error or would like to contribute to my plugin, contact me via the platforms below:
-- Discord: NhanAZ#9115
-- Xbox: NhanAZ
-- Zalo: @thanhnhanaz
-- FaceBook: fb.com/thanhnhanaz
-
-# License
-[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.html)
+**You can contact me directly via Discord NhanAZ#9115**
--- src/Main.php
+++ src/Main.php
@@ -8,39 +8,27 @@ use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use onebone\economyapi\EconomyAPI;
 use pocketmine\event\player\PlayerDeathEvent;
-use NhanAZ\RevivalTax\libs\JackMD\UpdateNotifier\UpdateNotifier;
 
-class Main extends PluginBase implements Listener
-{
+class Main extends PluginBase implements Listener {
 
-	public function onLoad() : void
-	{
-		UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
-	}
-
-	public function onEnable() : void
-	{
+	protected function onEnable(): void {
 		$this->saveDefaultConfig();
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
-	public function onPlayerDeath(PlayerDeathEvent $event)
-	{
-
-		$Player = $event->getPlayer();
-
-		$TaxPercent = $this->getConfig()->get('TaxPercent');
-
-		$Money = EconomyAPI::getInstance()->myMoney($Player);
-		$HandleTax = round(($Money * $TaxPercent / 100), 1);
-		if ($Money >= 1) {
-			EconomyAPI::getInstance()->reduceMoney($Player->getName(), $HandleTax);
-
-			$Search = ['{TaxMoney}', '{TaxPercent}'];
-			$Replace = [$HandleTax, $TaxPercent];
-			$Subject = $this->getConfig()->get('TaxNotice');
-
-			$Player->sendMessage(str_replace($Search, $Replace, $Subject));
+	public function onPlayerDeath(PlayerDeathEvent $event): void {
+		$player = $event->getPlayer();
+		$taxPercent = $this->getConfig()->get("taxPercent", 10);
+		$money = EconomyAPI::getInstance()->myMoney($player);
+		$handleTax = round(($money * $taxPercent / 100), 1);
+		if ($money >= 1) {
+			EconomyAPI::getInstance()->reduceMoney($player->getName(), $handleTax);
+			$replacements = [
+				"{taxMoney}" => $handleTax,
+				"{taxPercent}" => $taxPercent,
+			];
+			$subject = $this->getConfig()->get("taxMessage", "§aYou have been deducted §e{taxMoney} §arevival tax! §f(§bTax Percent: §c{taxPercent}%§f)§r");
+			$player->sendMessage(str_replace(array_keys($replacements), $replacements, $subject));
 		}
 	}
 }
